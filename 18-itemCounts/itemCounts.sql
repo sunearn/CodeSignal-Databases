27db39c550086e7b4ec8871276be89668c0948bf
sunearn <38371324+sunearn@users.noreply.github.com>
2024-12-20 14:39:05 +0530

    Update itemCounts.sql

--- 18-itemCounts/itemCounts.sql
+++ 18-itemCounts/itemCounts.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE itemCounts()
 BEGIN
-    SELECT item_name, item_type, count(*) as item_count
+    SELECT item_name, item_type, count(id) as item_count
     FROM availableItems
     GROUP BY item_name, item_type
     ORDER BY item_type, item_name;
