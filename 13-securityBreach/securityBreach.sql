1482cee369c73cee103c571d4c35ddc29bbeaa16
sunearn <38371324+sunearn@users.noreply.github.com>
2024-12-20 12:03:09 +0530

    Update securityBreach.sql

--- 13-securityBreach/securityBreach.sql
+++ 13-securityBreach/securityBreach.sql
@@ -1,7 +1,6 @@
 CREATE PROCEDURE securityBreach()
 BEGIN
-SELECT first_name, second_name, attribute
-FROM users
-WHERE BINARY attribute LIKE  CONCAT('_%', CONCAT('\%',first_name, '\_',second_name,'\%'), '%');
-	
+	SELECT first_name, second_name, attribute
+	FROM users
+	WHERE attribute LIKE BINARY CONCAT('_%\%',first_name,'\_',second_name,'\%%');
 END
