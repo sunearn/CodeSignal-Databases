1b6bf593ff11678206c4a703dcc3e699aa3ad65b
sunearn <38371324+sunearn@users.noreply.github.com>
2024-12-20 11:38:13 +0530

    Update gradeDistribution.sql

--- 09-gradeDistribution/gradeDistribution.sql
+++ 09-gradeDistribution/gradeDistribution.sql
@@ -1,6 +1,7 @@
 CREATE PROCEDURE gradeDistribution()
 BEGIN
 	SELECT Name, ID FROM Grades 
-	WHERE Final > Midterm1*.25+Midterm2*.25+Final*.5 && Midterm1*.5+Midterm2*.5
-    ORDER BY LEFT(Name, 3), ID ASC;
+	WHERE Final > Midterm1*.25+Midterm2*.25+Final*.5 
+	  AND Final > Midterm1*.5+Midterm2*.5
+    ORDER BY LEFT(Name, 3), ID;
 END
