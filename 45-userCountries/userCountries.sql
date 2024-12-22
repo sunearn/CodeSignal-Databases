bbe39d4ebc1c3c71282e5692117bac85871d83f5
sunearn <38371324+sunearn@users.noreply.github.com>
2024-12-22 21:51:41 +0530

    Update userCountries.sql

--- 45-userCountries/userCountries.sql
+++ 45-userCountries/userCountries.sql
@@ -1,6 +1,7 @@
 CREATE PROCEDURE userCountries()
 BEGIN
 	SELECT users.id, IFNULL(cities.country, 'unknown') as country
+	             --COALESCE(c.country, 'unknown') AS country
     FROM users
     LEFT JOIN cities
     ON users.city = cities.city
