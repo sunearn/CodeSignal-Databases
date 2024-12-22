ad4d3f304eda63bf12f672bdd5c0a63744ac28cb
sunearn <38371324+sunearn@users.noreply.github.com>
2024-12-22 18:38:05 +0530

    Update netIncome.sql

--- 41-netIncome/netIncome.sql
+++ 41-netIncome/netIncome.sql
@@ -3,4 +3,5 @@ BEGIN
   SELECT YEAR(date) as year, QUARTER(date) as quarter, SUM(profit)-SUM(loss) as net_profit
     FROM accounting
     GROUP BY YEAR(date), QUARTER(date);
+    -- ORDER BY year, quarter;
 END
